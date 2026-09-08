0edd32f93545250a7c25e1062c89bc97ae9de1d9
yashr15122-source <yashr15122@gmail.com>
2026-09-08 11:33:06 +0530

    vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/ngl-anonymous/"
+  base: process.env.VITE_BASE_PATH || "/"
 });
